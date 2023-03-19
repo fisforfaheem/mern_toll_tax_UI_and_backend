41ad01ed24fc020d1d8fef99310ceb96e8ed4b98
Faheem Ahmad <fisforfaheem@gmail.com>
2023-03-19 21:12:06 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+Faheem Ahmad MERN 6
---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+require('./core/app_config')(app,  express);
+
+//default route
+app.get('/', (request, response) => {
+  response.send('Server running...');
+});
+
+// other routes
+const routes = require('./routes/');
+routes.forEach((route)=>{
+  app.use(route.url, route.actions);
+});
+
+//error handling
+require('./core/error_handler')(app);
+
+// Starting server
+app.listen(8000, () => {
+  console.log('Server started on port 8000');
+} );
